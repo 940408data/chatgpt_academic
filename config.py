7cdcf60ea55bf43c486475b0c53c522c66be8239
huangxu3 <huangxu3@newhope.cn>
2023-06-08 18:06:32 +0800

    update

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-# API_KEY = "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" 此key无效
-API_KEY = "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"
+# API_KEY = "" 此key无效
+API_KEY = ""
 API_URL = "https://api.openai.com/v1/chat/completions"
 
 # 改为True应用代理
